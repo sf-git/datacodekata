399b163c970eb356f677ab57b1a2650c7e4ed8dd
sf-git <sf-git@github.com>
2020-10-04 21:26:19 +1100

    DCK-3: Fixing docker permissions

--- docker_run_csv.sh
+++ docker_run_csv.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-MSYS2_ARG_CONV_EXCL="*" docker run -ti --rm -v "$(pwd)":/work  --entrypoint csv_cli dck_problem1:0.0.1 --spec_file spec.json --fwf_file o.txt --csv_file o.csv
+MSYS2_ARG_CONV_EXCL="*" docker run -ti --rm -v "$(pwd)":/work --user $(id -u):$(id -g)  --entrypoint csv_cli dck_problem1:0.0.1 --spec_file spec.json --fwf_file o.txt --csv_file o.csv
--- docker_run_fwf.sh
+++ docker_run_fwf.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-MSYS2_ARG_CONV_EXCL="*" docker run -ti --rm -v "$(pwd)":/work  --entrypoint fwf_cli dck_problem1:0.0.1 --spec_file spec.json --fwf_file o.txt -n 15
+MSYS2_ARG_CONV_EXCL="*" docker run -ti --rm -v "$(pwd)":/work --user $(id -u):$(id -g)  --entrypoint fwf_cli dck_problem1:0.0.1 --spec_file spec.json --fwf_file o.txt -n 15
